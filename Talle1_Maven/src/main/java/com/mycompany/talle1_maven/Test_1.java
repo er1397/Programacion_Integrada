87f8cd0dd5828cf93e9d6632b28737e35cddcb31
BOG-A209-E-022 <BOG-A209-E-022@RGH-LS6-22.UMD.LOCAL.EDU>
2019-02-09 08:04:16 -0500

    Taller_1 Maven

---
+++ Talle1_Maven/src/main/java/com/mycompany/talle1_maven/Test_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.talle1_maven;
+import org.joda.time.LocalTime;
+
+
+
+/**
+ *
+ * @author BOG-A209-E-022
+ */
+public class Test_1 {
+    
+    public static void main(String [] args){
+        
+        LocalTime tiempo = new LocalTime();
+        System.out.println("El tiempo es: " + tiempo);
+    }
+    
+}
